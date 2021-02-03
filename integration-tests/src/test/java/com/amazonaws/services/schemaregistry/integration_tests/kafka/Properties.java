bc3b754be12ff8c83272366e0c3fd8ad161f1f85
Rushin <rushnaik@amazon.com>
2021-02-02 17:33:25 -0800

    Update the region endpoint and docker image.

--- integration-tests/src/test/java/com/amazonaws/services/schemaregistry/integration_tests/kafka/Properties.java
+++ integration-tests/src/test/java/com/amazonaws/services/schemaregistry/integration_tests/kafka/Properties.java
@@ -2,8 +2,8 @@ package com.amazonaws.services.schemaregistry.integration_tests.kafka;
 
 public class Properties {
     // Glue Service Endpoint
-    public static final String ENDPOINT = "https://glue.us-east-1.amazonaws.com";
-    public static final String REGION = "us-east-1";
+    public static final String ENDPOINT = "https://glue.us-east-2.amazonaws.com";
+    public static final String REGION = "us-east-2";
 
     private Properties() {
     }
