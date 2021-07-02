50d700a1178a4ac2a7bdef54bee8cf8812b89c54
mohit <mpaliwa@amazon.com>
2021-07-02 13:55:00 -0700

    Fix checkstyle build error and reduce tests for canaries

--- integration-tests/run-canary-tests.sh
+++ integration-tests/run-canary-tests.sh
@@ -1,9 +1,10 @@
 # Fail bash if any command fails
 set -e
 
-# Start Kafka using docker commnand asynchronously
+# Start Kafka using docker command asynchronously
 docker-compose up &
 sleep 10
 
 # Run mvn tests by downloading latest snapshot dependency versions
-mvn verify -Psurefire -U -X
+cd .. && mvn "-Dit.test=GlueSchemaRegistryKinesisIntegrationTest#testProduceConsumeWithKPLAndKCL,
+GlueSchemaRegistryKafkaIntegrationTest#testProduceConsumeWithSchemaRegistryMultiThreaded"  --file integration-tests/pom.xml verify -Psurefire -U
