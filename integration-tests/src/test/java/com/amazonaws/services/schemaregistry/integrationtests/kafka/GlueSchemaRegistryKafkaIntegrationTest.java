3c794c8338400dda2b35f8598044915b01af75fa
Øyvind Samuelsen <samuelsen.1995@gmail.com>
2022-01-11 20:20:14 +0100

    remove url-http-client dependency from tests

--- integration-tests/src/test/java/com/amazonaws/services/schemaregistry/integrationtests/kafka/GlueSchemaRegistryKafkaIntegrationTest.java
+++ integration-tests/src/test/java/com/amazonaws/services/schemaregistry/integrationtests/kafka/GlueSchemaRegistryKafkaIntegrationTest.java
@@ -35,7 +35,6 @@ import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.glue.GlueClient;
 import software.amazon.awssdk.services.glue.model.Compatibility;
@@ -108,8 +107,6 @@ public class GlueSchemaRegistryKafkaIntegrationTest {
                 .credentialsProvider(awsCredentialsProvider)
                 .region(Region.of(REGION))
                 .endpointOverride(new URI(SCHEMA_REGISTRY_ENDPOINT_OVERRIDE))
-                .httpClient(UrlConnectionHttpClient.builder()
-                                    .build())
                 .build();
 
         for (String schemaName : schemasToCleanUp) {
