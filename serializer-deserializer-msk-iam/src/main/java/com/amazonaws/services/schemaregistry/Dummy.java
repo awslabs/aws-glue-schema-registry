7a8c74faaa3b70436166ccef06b7f840427efc04
George Yang <haoqyang@amazon.com>
2023-10-18 13:24:05 -0700

    Add a dummy class in the serializer-deserializer-msk-iam module for javadoc and source jar generation (#256)

---
+++ serializer-deserializer-msk-iam/src/main/java/com/amazonaws/services/schemaregistry/Dummy.java
@@ -0,0 +1,7 @@
+package com.amazonaws.services.schemaregistry;
+
+/**
+ * This is just a dummy file created so the Javadoc jar and Source jar will be successfully generated for release
+ * */
+public class Dummy {
+}
