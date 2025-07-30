3fef37f421998326521a384f9d005b03896fcc3c
Saurab Sirpurkar <isaurab@amazon.com>
2025-07-30 18:44:04 +0000

    Add KafkaFlow package references and update deserializer/serializer files

--- native-schema-registry/csharp/AWSGsrSerDe/AWSGsrSerDe/KafkaFlow/GsrKafkaFlowProtobufDeserializer.cs
+++ native-schema-registry/csharp/AWSGsrSerDe/AWSGsrSerDe/KafkaFlow/GsrKafkaFlowProtobufDeserializer.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using AWSGsrSerDe.deserializer;
 using AWSGsrSerDe.common;
 using Google.Protobuf;
--- native-schema-registry/csharp/AWSGsrSerDe/AWSGsrSerDe/KafkaFlow/GsrKafkaFlowProtobufSerializer.cs
+++ native-schema-registry/csharp/AWSGsrSerDe/AWSGsrSerDe/KafkaFlow/GsrKafkaFlowProtobufSerializer.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using AWSGsrSerDe.serializer;
 using AWSGsrSerDe.common;
 using Google.Protobuf;
