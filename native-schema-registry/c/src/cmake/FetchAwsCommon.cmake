ef3cd1d4774fc2a29c171d0c8835655a643e3fb2
Michael Knox <mrknox@amazon.com>
2025-08-06 00:35:49 +0000

    updating cmake to also build maven parent to prevent cyclic build

--- native-schema-registry/c/src/cmake/FetchAwsCommon.cmake
+++ native-schema-registry/c/src/cmake/FetchAwsCommon.cmake
@@ -5,7 +5,7 @@ set(AWS_C_COMMON aws-c-common)
 FetchContent_Declare(
         AWS_C_COMMON
         GIT_REPOSITORY https://github.com/awslabs/aws-c-common.git
-        GIT_TAG        v0.7.4
+        GIT_TAG        v0.12.4
         GIT_SHALLOW    1
 )
 
