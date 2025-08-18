8956e72704e98ecca78f89ba3ca1b12b96a5b52e
Michael Knox <mrknox@amazon.com>
2025-08-18 21:33:24 +0000

    cleaning up golang pr-branch

--- native-schema-registry/golang/pkg/GsrSerDe/cgo_flags.go
+++
@@ -1,9 +0,0 @@
-package GsrSerDe
-
-/*
-#cgo CFLAGS: -w
-#cgo CFLAGS: -I../../../c/include
-#cgo LDFLAGS: -Wl,-rpath,${SRCDIR}/../../lib
-#cgo LDFLAGS: -L../../lib -lnativeschemaregistry -lnative_schema_registry_c -lnative_schema_registry_c_data_types -laws_common_memalloc
-*/
-import "C"
