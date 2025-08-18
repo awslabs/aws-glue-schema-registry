5544b9ed580e3dad7c66226e14de4e4aaea0a375
Michael Knox <mrknox@amazon.com>
2025-08-18 21:41:51 +0000

    Basic structure and makefile for golang

---
+++ native-schema-registry/golang/go.mod
@@ -0,0 +1,25 @@
+module github.com/awslabs/aws-glue-schema-registry/native-schema-registry/golang
+
+go 1.20
+
+require (
+	github.com/hamba/avro/v2 v2.25.1
+	github.com/stretchr/testify v1.10.0
+	github.com/xeipuuv/gojsonschema v1.2.0
+	google.golang.org/protobuf v1.36.5
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ native-schema-registry/golang/gsrserde.go
@@ -0,0 +1,25 @@
+package gsrserde
+
+import (
+	"github.com/awslabs/aws-glue-schema-registry/native-schema-registry/golang/pkg/gsrserde-go/common"
+	"github.com/awslabs/aws-glue-schema-registry/native-schema-registry/golang/pkg/gsrserde-go/deserializer"
+	"github.com/awslabs/aws-glue-schema-registry/native-schema-registry/golang/pkg/gsrserde-go/serializer"
+)
+
+// KafkaDeserializer type alias for easy access from main package
+type KafkaDeserializer = deserializer.Deserializer
+
+// KafkaSerializer type alias for easy access from main package
+type KafkaSerializer = serializer.Serializer
+
+// NewKafkaDeserializer creates a new Kafka deserializer instance
+// This is a convenience function that mirrors the C# implementation structure
+func NewKafkaDeserializer(config *common.Configuration) (*KafkaDeserializer, error) {
+	return deserializer.NewDeserializer(config)
+}
+
+// NewKafkaSerializer creates a new Kafka serializer instance
+// This is a convenience function that mirrors the C# implementation structure
+func NewKafkaSerializer(config *common.Configuration) (*KafkaSerializer, error) {
+	return serializer.NewSerializer(config)
+}
