8cc0c2bc0e3c266226e9e41068a793576ab38c71
Saurab Sirpurkar <isaurab@amazon.com>
2025-08-19 19:05:18 +0000

    Enhance error message in SchemaByDefinitionFetcher to include exception cause and update corresponding test case

--- common/src/test/java/com/amazonaws/services/schemaregistry/common/SchemaByDefinitionFetcherTest.java
+++ common/src/test/java/com/amazonaws/services/schemaregistry/common/SchemaByDefinitionFetcherTest.java
@@ -192,6 +192,8 @@ public class SchemaByDefinitionFetcherTest {
                 .getORRegisterSchemaVersionId(userSchemaDefinition, schemaName, dataFormatName, getMetadata()));
         assertTrue(
             exception.getMessage().contains("Exception occurred while fetching or registering schema definition"));
+        assertTrue(
+            exception.getMessage().contains("Error: Unknown"));
     }
 
     @Test
