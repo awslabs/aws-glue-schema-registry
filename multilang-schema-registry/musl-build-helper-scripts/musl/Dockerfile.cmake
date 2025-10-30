ce2b3ce36efe9677bc4dfb0c24b877153983a507
Saurab Sirpurkar <isaurab@amazon.com>
2025-10-30 13:06:20 +0000

    Dockerfile changes and musl

--- multilang-schema-registry/musl-build-helper-scripts/musl/Dockerfile.cmake
+++ multilang-schema-registry/musl-build-helper-scripts/musl/Dockerfile.cmake
@@ -1,10 +1,13 @@
-# Support multi-platform builds
+# Support multi-platform builds - ARG must be before FROM
 ARG TARGETPLATFORM
-FROM --platform=$TARGETPLATFORM mcr.microsoft.com/dotnet/sdk:8.0-alpine
+FROM mcr.microsoft.com/dotnet/sdk:8.0-alpine
 
 # Set UTF-8 locale to handle Unicode filenames (including emojis)
 ENV LANG=C.UTF-8
 ENV LC_ALL=C.UTF-8
+
+# Re-import TARGETPLATFORM after FROM
+ARG TARGETPLATFORM
 
 RUN apk add \
 git curl ca-certificates unzip zip findutils \
